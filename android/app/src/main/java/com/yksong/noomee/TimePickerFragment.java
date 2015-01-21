6b17444a440071c76800da43bcb24acca3ad82fa
Qi Feng Liu <qfliu@uwaterloo.ca>
2015-01-21 01:06:14 -0500

    Add post event ui.

---
+++ android/app/src/main/java/com/yksong/noomee/TimePickerFragment.java
@@ -0,0 +1,46 @@
+package com.yksong.noomee;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by Ed on 16/01/2015.
+ */
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    NewEventActivity callingActivity;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        callingActivity = (NewEventActivity) getActivity();
+
+        // Use the current time as the default values for the picker
+        int hour = callingActivity.getEventTimeHour();
+        int minute = callingActivity.getEventTimeMinute();
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        TextView timeText = (TextView) callingActivity.findViewById(R.id.Time);
+        callingActivity.onUserSetTimeInfo(timeText, "Time: "+pad(hourOfDay)+" : "+pad(minute));
+        callingActivity.setEventTime(hourOfDay, minute);
+    }
+
+    private static String pad(int c) {
+        if (c >= 10)
+            return String.valueOf(c);
+        else
+            return "0" + String.valueOf(c);
+    }
+}
