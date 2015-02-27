8a49676127c337ed3d0b37a811e507f82c270bd1
Qi Feng Liu <qfliu@uwaterloo.ca>
2015-02-26 20:16:37 -0500

    initial change to pull from Parse

--- android/app/src/main/java/com/yksong/noomee/views/ActivitiesView.java
+++ android/app/src/main/java/com/yksong/noomee/views/ActivitiesView.java
@@ -19,6 +19,8 @@ import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 import com.yksong.noomee.R;
 
 import java.text.SimpleDateFormat;
@@ -26,11 +28,15 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import com.yksong.noomee.util.APICallback;
+import com.yksong.noomee.util.ParseAPI;
 
 /**
  * Created by esong on 2015-01-01.
  */
 public class ActivitiesView extends FrameLayout {
+    private RecyclerView mRecList;
+
     public ActivitiesView(Context context) {
         this(context, null);
     }
@@ -58,32 +64,55 @@ public class ActivitiesView extends FrameLayout {
     protected void onFinishInflate() {
         super.onFinishInflate();
 
-        RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
-        recList.setHasFixedSize(true);
+
+        mRecList = (RecyclerView) findViewById(R.id.cardList);
+        mRecList.setHasFixedSize(true);
         LinearLayoutManager llm = new LinearLayoutManager(getContext());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recList.setLayoutManager(llm);
+        mRecList.setLayoutManager(llm);
 
-        ContactAdapter ca = new ContactAdapter(createList(30));
-        recList.setAdapter(ca);
+        ContactAdapter ca = new ContactAdapter(new ArrayList<EatingEvent>());
+        mRecList.setAdapter(ca);
+        getEventList();
     }
 
-    private List<EatingEvent> createList(int size) {
+    private void createList(List<EatingEvent> result){
+        ContactAdapter ca = new ContactAdapter(result);
+        mRecList.setAdapter(ca);
+    }
 
-        List<EatingEvent> result = new ArrayList<EatingEvent>();
-        for (int i=1; i <= size; i++) {
-            EatingEvent ci = new EatingEvent();
 
-            ci.location="Fucking Tomato";
-            result.add(ci);
-        }
+    private void getEventList() {
+        ParseAPI.getMyAndFriendsEvents( ParseUser.getCurrentUser(),
+                new APICallback<List<ParseObject>>() {
+                    @Override
+                    public void run(List<ParseObject> parseObj) {
+                        for(int i=0;i<parseObj.size();i++)
+                        {
+                            ArrayList<ParseObject> users = (ArrayList<ParseObject>)parseObj.get(i).get("users");
+                            for(int j=0;j<users.size();j++)
+                                System.out.println("user "+j+": "+users.get(j));
+                            System.out.println("cnm2: "+parseObj.get(i).get("scheduledAt"));
+                        }
 
-        return result;
+                        List<EatingEvent> result = new ArrayList<EatingEvent>();
+                        for (int i=1; i <= 20; i++) {
+                            EatingEvent ci = new EatingEvent();
+
+                            ci.location="Fucking Tomato";
+                            result.add(ci);
+                        }
+                        createList(result);
+                    }
+                },
+                20);
     }
 
     public class EatingEvent {
+        protected String creator;
         protected String location;
         protected SimpleDateFormat time;
+        protected ArrayList<String> users;
     }
 
     public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactViewHolder> {
